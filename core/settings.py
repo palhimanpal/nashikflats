e6509a9b1c114e18b695f93a0cacbd41a46a1a65
palhimanpal <palhimanpal786@gmail.com>
2022-02-08 22:17:42 +0530

    [update] add Database router for user db, custom user model

--- core/settings.py
+++ core/settings.py
@@ -96,6 +96,8 @@ DATABASES = {
     }
 }
 
+DATABASE_ROUTERS = ['user.router.AuthRouter',]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -158,3 +160,5 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.UserAccount'
